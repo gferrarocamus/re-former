93f548195575f9491af25fe00aab11a441d6c336
Giuliana Ferraro <giulianaferrarocamus@gmail.com>
2019-04-16 18:49:49 +0200

    fix for editing and displaying errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'users#new'
-  resources :users, only: [:create, :new]
-  get 'users/:id/edit', to: 'users#edit'
-  patch 'users.:id', to: 'users#update'
+  resources :users, only: [:create, :new, :edit, :update]
+  #get 'users/:id/edit', to: 'users#edit'
+  #patch 'users/:id', to: 'users#update'
 end
